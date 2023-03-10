0acd35f987cc6591e5bf43e45a7079c1cfea5bb3
nqhuy19 <112528789+nqhuy19@users.noreply.github.com>
2023-03-10 09:56:23 +0700

    add multer middleware

--- backend/src/api/event/event.router.ts
+++ backend/src/api/event/event.router.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import EventController from "./event.controller";
+import multer from "multer";
 
 const eventRouter = express.Router();
+const upload = multer();
 
 eventRouter.get("/:eventId", EventController.getEventById);
-eventRouter.post("/", EventController.createEvent);
+eventRouter.post("/", upload.none(), EventController.createEvent);
 eventRouter.patch("/:eventId", EventController.updateEvent);
 eventRouter.delete("/:eventId", EventController.deleteEvent);
 
