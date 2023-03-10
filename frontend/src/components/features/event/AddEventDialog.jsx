6bdc2379b6f84441f6d16e9836e9d7891df9bbcd
nqhuy19 <112528789+nqhuy19@users.noreply.github.com>
2023-03-10 09:55:39 +0700

    set start end time on form

---
+++ frontend/src/components/features/event/AddEventDialog.jsx
@@ -0,0 +1,123 @@
+import { Dialog, DialogContent, DialogActions } from "@mui/material";
+import { Button, TextField, Checkbox, FormControlLabel } from "@mui/material";
+import { Grid } from "@mui/material";
+import RemoveIcon from '@mui/icons-material/Remove';
+import { LocalizationProvider, DesktopDatePicker, TimePicker } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { useFormik } from 'formik';
+import dayjs from "dayjs";
+import * as Yup from 'yup';
+
+export default function AddEventDialog({ open, setOpen }) {
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      title: '',
+      start: new Date(),
+      end: new Date(),
+      allDay: false,
+      location: '',
+      description: '',
+      startDate: dayjs(new Date()),
+      endDate: dayjs(new Date()),
+      startTime: dayjs(new Date()),
+      endTime: dayjs(new Date())
+    },
+    validationSchema: Yup.object({
+      title: Yup.string().required(),
+      start: Yup.date().required(),
+      end: Yup.date().required(),
+      allDay: Yup.boolean(),
+      location: Yup.string(),
+      description: Yup.string(),
+    }),
+    onSubmit: values => {
+      let { allDay, startDate, endDate, startTime, endTime } = values
+      let { $y, $M, $D } = startDate;
+      var { $H, $m } = startTime;
+      values.start = allDay ? new Date($y, $M, $D) : new Date($y, $M, $D, $H, $m);
+      var { $H, $m } = endTime;
+      values.end = allDay ? new Date($y, $M, $D, $H, $m) : endDate.$d;
+      console.log(values);
+    },
+  });
+
+  return (
+    <Dialog fullWidth={true} maxWidth="sm"
+      open={open} onClose={handleClose}
+      sx={{ padding: 2 }}
+    >
+      <form onSubmit={formik.handleSubmit}>
+        <DialogContent>
+          <TextField id="title" type="text" placeholder="Add event's title" variant="standard" fullWidth autoFocus={true}
+            sx={{ mb: 4, lineHeight: "40px" }}
+            inputProps={{ style: { fontSize: 30 } }}
+            {...formik.getFieldProps('title')}
+          />
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <Grid container alignItems="center">
+              <DesktopDatePicker sx={{ mr: 2, width: "50%" }}
+                name="startDate"
+                inputFormat="MM/DD/YYYY"
+                value={dayjs(formik.values.startDate)}
+                onChange={value => {
+                  formik.setFieldValue("startDate", value.$d);
+                }}
+              />
+              {
+                formik.values.allDay ?
+                  <DesktopDatePicker
+                    name="endDate"
+                    inputFormat="DD/MM/YYYY"
+                    value={dayjs(formik.values.endDate)}
+                    onChange={value => {
+                      formik.setFieldValue("endDate", value);
+                    }}
+                  />
+                  :
+                  <Grid container alignItems="center" display="inline-flex" sx={{ width: "45%" }}>
+                    <TimePicker sx={{ width: "45%" }}
+                      name="startTime"
+                      value={formik.values.startTime}
+                      onChange={value => {
+                        formik.setFieldValue("startTime", value);
+                      }} />
+                    <RemoveIcon />
+                    <TimePicker sx={{ width: "45%" }}
+                      name="endTime"
+                      value={formik.values.endTime}
+                      onChange={value => {
+                        formik.setFieldValue("endTime", value);
+                      }}
+                    />
+                  </Grid>
+              }
+            </Grid>
+          </LocalizationProvider>
+          <FormControlLabel label="All day" control={
+            <Checkbox checked={formik.values.allDay}
+              sx={{ my: 2 }}
+              onClick={() => { formik.setFieldValue("allDay", !formik.values.allDay) }}
+            />
+          }
+          />
+          <TextField type="text" id="location" placeholder="Event's location" fullWidth
+            sx={{ mb: 2 }}
+            {...formik.getFieldProps('location')}
+          />
+          <TextField type="text" id="description" name="description" placeholder="Description" rows={3} multiline fullWidth
+            {...formik.getFieldProps('description')}
+          />
+        </DialogContent>
+        <DialogActions sx={{ px: 3, pb: 3 }}>
+          <Button onClick={handleClose} variant="outlined">Cancel</Button>
+          <Button variant="contained" type="submit">Save</Button>
+        </DialogActions>
+      </form>
+    </Dialog>
+  );
+}
--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,7 +1,13 @@
-import { Grid, Box } from "@mui/material";
+import { useState } from "react";
+import { Grid, Box, Stack } from "@mui/material";
+import { Button, Typography } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 import Calendar from "../components/features/event/Calendar";
+import AddEventDialog from "../components/features/event/AddEventDialog";
 
 export default function Main() {
+
+    const [showDialog, setShowDialog] = useState(false);
 
     return (
         <Grid container spacing={2} flexDirection="row-reverse">
@@ -11,7 +17,17 @@ export default function Main() {
                 </Box>
             </Grid>
             <Grid item component="aside" xs={12} md={4}>
-                <h1>Event list</h1>
+                <Stack direction="row" justifyContent="space-between" paddingX={3}>
+                    <Typography variant="h6" display={"inline-block"}>
+                        Events
+                    </Typography>
+                    <Button variant="contained" startIcon={<AddIcon />}
+                        onClick={() => { setShowDialog(true) }}
+                    >
+                        Add
+                    </Button>
+                    <AddEventDialog open={showDialog} setOpen={setShowDialog} />
+                </Stack>
             </Grid>
         </Grid>
     );
