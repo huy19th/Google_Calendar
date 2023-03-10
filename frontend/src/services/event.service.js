8a31a2fb9de0768ef70633bf96aeb19cc1eae0a6
nqhuy19 <112528789+nqhuy19@users.noreply.github.com>
2023-03-10 10:28:47 +0700

    fix create event service

--- frontend/src/services/event.service.js
+++ frontend/src/services/event.service.js
@@ -3,6 +3,10 @@ import axios, { axiosJWT } from "../configs/axios";
 export default class eventService {
 
     static createEvent(values) {
-        return axiosJWT.post("/event", values);
+        return axiosJWT.post("/event", values, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        });
     }
 }
