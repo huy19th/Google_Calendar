5caa782772a18c1aa9e7b06989da22d13a3a0485
nqhuy19 <112528789+nqhuy19@users.noreply.github.com>
2023-03-11 10:23:51 +0700

    event slice

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import snackbarReducer from "./snackbar.slice";
 import userReducer from "./user.slice";
+import eventReducer from "./event.slice";
 
 export const store = configureStore({
   reducer: {
     snackbar: snackbarReducer,
-    user: userReducer
+    user: userReducer,
+    event: eventReducer
   },
 });
