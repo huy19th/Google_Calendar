abb4fe3a609fe03d8d0373507e46a17e352c08b5
nqhuy19 <112528789+nqhuy19@users.noreply.github.com>
2023-03-11 22:50:00 +0700

    refactor

--- frontend/src/components/ui/AddButton.jsx
+++ frontend/src/components/ui/AddButton.jsx
@@ -4,8 +4,7 @@ import AddIcon from "@mui/icons-material/Add";
 export default function AddButton(props) {
 
     return (
-        <Button variant="contained" startIcon={<AddIcon />} {...props}
-        >
+        <Button variant="contained" startIcon={<AddIcon />} {...props}>
             Add
         </Button>
     )
