452684920120f3281f0fb3561f7cd48123847d46
nqhuy19 <112528789+nqhuy19@users.noreply.github.com>
2023-03-12 08:10:33 +0700

    show user table

--- frontend/src/components/features/user/UserTable.jsx
+++ frontend/src/components/features/user/UserTable.jsx
@@ -1,49 +1,49 @@
-import * as React from 'react';
-import { DataGrid } from '@mui/x-data-grid';
+import { useState } from 'react';
+import Paper from '@mui/material/Paper';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TablePagination } from "@mui/material";
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { useSelector } from "react-redux";
 
-const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
-];
+export default function BasicTable() {
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+    const users = useSelector(state => state.user.users);
+    const [data, setData] = useState(users);
 
-export default function UserTable() {
-  return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        checkboxSelection
-      />
-    </div>
-  );
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>#</TableCell>
+                        <TableCell align="left">Username</TableCell>
+                        <TableCell align="left">Email</TableCell>
+                        <TableCell align="left">Role</TableCell>
+                        <TableCell align="center"></TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {users.map((item, index) => (
+                        <TableRow
+                            key={item._id}
+                            hover
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            className="table-row"
+                        >
+                            <TableCell component="th" scope="row">
+                                {index + 1}
+                            </TableCell>
+                            <TableCell align="left">{item.username}</TableCell>
+                            <TableCell align="left">{item.email}</TableCell>
+                            <TableCell align="left">{item.role}</TableCell>
+                            <TableCell align="center">
+                                <EditIcon className="table-row-action" sx={{visibility: "hidden"}} />
+                                <DeleteIcon className="table-row-action" sx={{visibility: "hidden"}} />
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
 }
---
+++ frontend/src/ultils/table.util.js
@@ -0,0 +1,32 @@
+class TableUtils {
+
+    static descendingComparator(a, b, orderBy) {
+        if (b[orderBy] < a[orderBy]) {
+            return -1;
+        }
+        if (b[orderBy] > a[orderBy]) {
+            return 1;
+        }
+        return 0;
+    }
+
+    static getComparator(order, orderBy) {
+        return order === 'desc'
+            ? (a, b) => this.descendingComparator(a, b, orderBy)
+            : (a, b) => -this.descendingComparator(a, b, orderBy);
+    }
+
+    static stableSort(array, comparator) {
+        const stabilizedThis = array.map((el, index) => [el, index]);
+        stabilizedThis.sort((a, b) => {
+          const order = comparator(a[0], b[0]);
+          if (order !== 0) {
+            return order;
+          }
+          return a[1] - b[1];
+        });
+        return stabilizedThis.map((el) => el[0]);
+    }
+}
+
+export const { descendingComparator } = TableUtils
