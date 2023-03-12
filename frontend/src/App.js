e190dce22d981adcb60cdcbe3ab42d9f7cd4f445
nqhuy19 <112528789+nqhuy19@users.noreply.github.com>
2023-03-12 09:47:21 +0700

    fix router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,19 +4,74 @@ import Main from "./pages/Main";
 import Events from "./pages/Events";
 import Users from "./pages/Users";
 import Snackbar from "./components/ui/Snackbar";
+import Backdrop from "./components/ui/Backdrop";
+import { useSelector, useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
+import { login, setUsers } from "./store/user.slice";
+import { setEvents } from "./store/event.slice";
+import UserService from "./services/user.service";
+import EventService from "./services/event.service";
 
 function App() {
+
+  const dispatch = useDispatch();
+
+  const [loading, setLoading] = useState(true);
+
+  const isLoggedIn = useSelector(state => state.user.isLoggedIn);
+
+  useEffect(() => {
+    if (!isLoggedIn && localStorage.getItem("token")) {
+      async function getData() {
+        try {
+          let user = (await UserService.getUserInfo()).data;
+          dispatch(login(user));
+          setLoading(false);
+          let users = (await UserService.getUserList()).data;
+          dispatch(setUsers(users));
+          let events = (await EventService.getEvents()).data;
+          dispatch(setEvents(events));
+        }
+        catch (err) {
+          console.log(err);
+          setLoading(false);
+        }
+      }
+      getData();
+    }
+    else setLoading(false);
+    // eslint-disable-next-line
+  }, []);
+
+  // if (loading) {
+  //   return (
+  //     <BrowserRouter>
+  //       <Routes>
+  //         <Route path="*" element={<Backdrop />} />
+  //       </Routes>
+  //     </BrowserRouter>
+  //   )
+  // }
 
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Main />} >
-            <Route index element={<Events />} />
-            <Route path="/users" element={<Users />} />
-          </Route>
-          <Route path="/login" element={<Login />} />
-          <Route path="*" element={<Navigate to="/" />} />
+          {
+            isLoggedIn ?
+              <>
+                <Route path="/" element={<Main />} >
+                  <Route index element={<Events />} />
+                  <Route path="/users" element={<Users />} />
+                </Route>
+                <Route path="*" element={<Navigate to="/" />} />
+              </>
+              :
+              <>
+                <Route path="/login" element={loading ? <Backdrop /> : <Login />} />
+                <Route path="*" element={loading ? <Backdrop /> : <Navigate to="/login" />} />
+              </>
+          }
         </Routes>
       </BrowserRouter>
       <Snackbar />
