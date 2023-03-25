55cbb77a36469d1f2ed3783c2f394772c0134095
nqhuy19 <112528789+nqhuy19@users.noreply.github.com>
2023-03-25 21:40:35 +0700

    set this & next month

--- frontend/src/store/event.slice.js
+++ frontend/src/store/event.slice.js
@@ -2,6 +2,8 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     events: [],
+    thisMonth: [],
+    nextMonth: []
 }
 
 export const eventSlice = createSlice({
@@ -10,10 +12,26 @@ export const eventSlice = createSlice({
     reducers: {
         setEvents: (state, action) => {
             state.events = action.payload
+        },
+        setThisMonth: (state) => {
+            let today = new Date();
+            let month = today.getMonth();
+            let year = today.getFullYear();
+            state.thisMonth = state.events.filter(item => {
+                return item.start.getMonth() === month && item.start.getFullYear() === year;
+            });
+        },
+        setNextMonth: (state) => {
+            let today = new Date();
+            let month = today.getMonth() + 1;
+            let year = today.getFullYear();
+            state.nextMonth = state.events.filter(item => {
+                return item.start.getMonth() === month && item.start.getFullYear() === year;
+            });
         }
     }
 })
 
-export const { setEvents } = eventSlice.actions;
+export const { setEvents, setThisMonth, setNextMonth } = eventSlice.actions;
 
 export default eventSlice.reducer;
